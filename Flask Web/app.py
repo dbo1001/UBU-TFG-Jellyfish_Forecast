cbf5d4f5136187da00fa03e6f506d8fa8a09a8be
psnti <52669634+psnti@users.noreply.github.com>
2020-03-19 21:18:45 +0100

    Se añaden puntos al mapa a partir de un GeoJson #22

--- Flask Web/app.py
+++ Flask Web/app.py
@@ -1,6 +1,9 @@
 from flask import Flask,render_template
 from flask_bootstrap import Bootstrap
 import folium
+import os
+import json
+
 
 app = Flask(__name__)
 Bootstrap(app)
@@ -23,7 +26,24 @@ def paginaContacto():
 
 @app.route('/pruebas')
 def index():
-    return render_template('pruebas.html')
+    start_coords = (-34.536267, -72.406639)
+    folium_map = folium.Map(location=start_coords, zoom_start=5)
+    gjson = os.path.join(app.root_path, 'geojson/prueba.geojson')
+    file = open(gjson,encoding='utf-8')
+    geo_data = json.load(file)
+    file.close()
+    print(geo_data)
+    
+    w = geo_data['geometry']
+    for x,y in enumerate(w['coordinates']):
+        print('-----------------------------------',x,y)
+        folium.Marker(
+            location=[y[0],y[1]],
+            popup=w["nombre"][x]
+        ).add_to(folium_map)
+        
+    folium_map.save('templates/mapita.html')
+    return render_template('mapas.html')
 
 if __name__ == '__main__':
     app.run(debug = True)
