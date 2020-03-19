c41b3d6fcfd3f6d5427eb9dad9df014e6ef57086
psnti <52669634+psnti@users.noreply.github.com>
2020-03-19 16:17:39 +0100

    Mejoras en interfaz y añadido mapa #22

--- Flask Web/app.py
+++ Flask Web/app.py
@@ -1,18 +1,29 @@
 from flask import Flask,render_template
+from flask_bootstrap import Bootstrap
+import folium
 
 app = Flask(__name__)
+Bootstrap(app)
 
 @app.route('/')
 def paginaPrincipal():
-    return render_template('welcome.html')
+    return render_template('index.html')
 
-@app.route('/pred')
+@app.route('/mapas')
 def paginaPrediccion():
-    return render_template('pred.html')
+    return render_template('mapas.html')
 
 @app.route('/contacto')
 def paginaContacto():
     return render_template('contacto.html')
+
+
+@app.route('/pruebas')
+def index():
+    start_coords = (-34.536267, -72.406639)
+    folium_map = folium.Map(location=start_coords, zoom_start=5)
+    folium_map.save('templates/mapita.html')
+    return render_template('mapas.html')
 
 if __name__ == '__main__':
     app.run(debug = True)
